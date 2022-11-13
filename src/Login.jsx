7f5cefeaad8e588d76f5e2c5dbd65d493c2ce2c2
nagudelo90academia <nagudelo90academia@gmail.com>
2022-11-13 18:48:42 -0500

    :sparkles: Creating login and register users for project frontendrecursoshumanos -- cycle 4 UTP

---
+++ src/Login.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+export const Login = (props) => {
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email);
+    }
+
+    return (
+        <div className="auth-form-container">
+            <h1> Recursos Humanos </h1>
+            <h2>Inicio de Sesión</h2>
+            <form className="login-form" onSubmit={handleSubmit}>
+                <label htmlFor="email">Email</label>
+                <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="tuemail@gmail.com" id="email" name="email" />
+                <label htmlFor="password">Clave</label>
+                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+                <button type="submit">Iniciar Sesión</button>
+            </form>
+            <button className="link-btn" onClick={() => props.onFormSwitch('register')}>No tienes una cuenta? Registrate aquí.</button>
+        </div>
+    )
+}
